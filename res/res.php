d1dc28d9f713d10ed58fef762bbcd134ce2dc088
teymzz <teymss@gmail.com>
2026-08-27 14:42:47 +0100

    Spoova update [removed redundant files]

--- res/res.php
+++ res/res.php
@@ -8,7 +8,7 @@ Ress::new('res/assets/') //base directory
      ->urx('res/main/js/local/Theme.js')->named('ThemeJs') //add your resources
      ->url('libraries/highlight/highlight.js')->named('HighlightJS') //add your resources
      ->urx('res/main/js/local/checkBox/checkBox.js => type:module')->named('checkboxJS') //add your resources
-     ->urx('res/main/js/local/interval.js => type:module')->named('intervalJS') // for spoovacs doc
+     ->urx('res/main/js/local/interval.js => type:module')->named('intervalJS') // for spoova doc
 
      ->bindTo('headers', ['animateCSS','ThemeJs','HighlightJS'])
     
